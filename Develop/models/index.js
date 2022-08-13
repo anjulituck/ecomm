8ec59dcdbe3efa73d0d27112cb770132bff111a3
anjulituck@Anjulis-iMac.local <anjulituck@Anjulis-iMac.local>
2022-08-12 20:40:07 -0500

    added info the model index but need to check on through models part for the last two

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,28 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category,{
+  foreignKey:'category_id'
+})
 
 // Categories have many Products
+Category.hasMany(Product,{
+  foreignKey:'product_id'
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongToMany(Tag,{
+  through:ProductTag,
+  as:'tag_id',
+  foreignKey:'product_id'
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongToMany(Product,{
+  through:ProductTag,
+  as:'product_id',
+  foreignKey:'product_id'
+})
 
 module.exports = {
   Product,
